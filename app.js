f8ce68c49823c0b100229aa1d97d433cd65a98e9
Martín <mcflytix333@gmail.com>
2023-09-19 19:15:59 -0300

    agregando función multiplicar

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const process=require('process')
 const suma=require('./suma')
 const resta=require('./resta')
-const dividir=require('./dividir')
-const multiplicar=require ('./multiplicar')
+//const dividir=require('./dividir')
+const multiplicar = require ('./multiplicar')
 const {argv}= process
 
 function calculadora(comando,a,b) {
