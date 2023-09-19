b96af95e4d29ea5a1547357aed176b3e2c679bcc
Martín <mcflytix333@gmail.com>
2023-09-19 18:53:45 -0300

    agregando función suma

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const process=require('process')
-const suma=require('require')
-const resta=require('./resta')
-const dividir=require('./dividir')
-const multiplicar=require ('./multiplicar')
-const {argv}= process
+const suma = require('./suma')
+//const resta=require('./resta')
+//const dividir=require('./dividir')
+//const multiplicar=require ('./multiplicar')
+const { argv }= process
 
 function calculadora(comando,a,b) {
     switch (comando.toLowerCase()) {
         case "sumar":
             return suma(a,b)
-            break;
+            
         case "restar":
             return resta(a,b)
-            break
         case "multiplicar":
             return multiplicar(a,b)
-            break
         case "dividir":
             return dividir(a,b)
         default:
@@ -23,4 +21,4 @@ function calculadora(comando,a,b) {
     }
     
 }
-console.log(calculadora(arg[2],+argv[3],+argv[4]))
+console.log(calculadora(argv[2],+argv[3],+argv[4]))
