33c5ecd98eeac9852b0df510ea53a6d73b8e82d3
Gaston Nicolás Díaz <gastondiazn@gmail.com>
2023-09-19 18:25:14 -0300

    interface terminada

---
+++ app.js
@@ -0,0 +1,25 @@
+const process=require('process')
+const resta=require('./resta')
+const dividir=require('./dividir')
+const multiplicar=require ('./multiplicar')
+const {argv}= process
+
+function calculadora(comando,a,b) {
+    switch (comando.toLowerCase()) {
+        case "sumar":
+            return suma(a,b)
+            break;
+        case "restar":
+            return resta(a,b)
+            break
+        case "multiplicar":
+            return multiplicar(a,b)
+            break
+        case "dividir":
+            return dividir(a,b)
+        default:
+            break;
+    }
+    
+}
+console.log(calculadora(arg[2],+argv[3],+argv[4]))
